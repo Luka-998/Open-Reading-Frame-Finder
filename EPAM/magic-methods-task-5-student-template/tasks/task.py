e1c3f33ad5e79c71c3990d006aacffa8234e1db8
Luka Jasovic <jasovicluka1@gmail.com>
2026-05-29 12:01:03 +0200

    Done

--- EPAM/magic-methods-task-5-student-template/tasks/task.py
+++ EPAM/magic-methods-task-5-student-template/tasks/task.py
@@ -1,14 +1,16 @@
 import os
-import shutil
 import uuid
+from shutil import rmtree
 
 
 class TempDir:
     def __enter__(self):
         self.previous_dir = os.getcwd()
 
-        dirname = str(uuid.uuid4())
-        self.temp_dir = os.path.join(self.previous_dir, dirname)
+        self.temp_dir = os.path.join(
+            self.previous_dir,
+            str(uuid.uuid4())
+        )
 
         os.mkdir(self.temp_dir)
         os.chdir(self.temp_dir)
@@ -17,4 +19,4 @@ class TempDir:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         os.chdir(self.previous_dir)
-        shutil.rmtree(self.temp_dir)
+        rmtree(self.temp_dir)
