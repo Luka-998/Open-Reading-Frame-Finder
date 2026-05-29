b9fa9e0aa2f9e4a8f6cf744df5059b1301065ae6
Luka Jasovic <jasovicluka1@gmail.com>
2026-05-29 11:54:40 +0200

    done

--- EPAM/magic-methods-task-5-student-template/tasks/task.py
+++ EPAM/magic-methods-task-5-student-template/tasks/task.py
@@ -1,22 +1,26 @@
 import os
 import shutil
+from uuid import uuid4
+
 
 class TempDir:
     def __enter__(self):
-        current_dir = os.getcwd()
-        self.current_dir = current_dir
+        self.current_dir = os.getcwd()
         print(f"Current dir path: {os.getcwd()}")
-        os.mkdir(os.path.join(os.getcwd(),'new_dir'))
-        path = os.path.join(os.getcwd(),'new_dir')
-        os.chdir(path)
+        dirname = str(uuid4())
+
+        os.mkdir(os.path.join(os.getcwd(),dirname))
+        self.path = os.path.join(os.getcwd(),dirname)
+        os.chdir(self.path)
         print(f"New directory path at: {os.getcwd()}")
-       
+        return f"Current new path: {self.path}"
+    
     def __exit__(self,exc_type,exc_val,exc_tb):
-        print(f"Removing current dir: {os.getcwd()}")
+        print(f"Removing current dir: {self.path}")
         os.chdir(self.current_dir)
-        shutil.rmtree(os.path.join(os.getcwd(),'new_dir'))
+        shutil.rmtree(self.path)
         print(f"Current dir is now: {os.getcwd()}")
        
 
-with TempDir():
-    print("Yes")
+with TempDir() as result:
+    print(f"This is result string: {result}")
